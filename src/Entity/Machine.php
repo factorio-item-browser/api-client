4f8a76dad8761df0afa5f0364cd9b4eb838777e2
BluePsyduck <bluepsyduck@gmx.com>
2018-05-17 16:55:04 +0200

    Added entity for machines.

---
+++ src/Entity/Machine.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FactorioItemBrowser\Api\Client\Entity;
+
+use BluePsyduck\Common\Data\DataContainer;
+use FactorioItemBrowser\Api\Client\Constant\EntityType;
+
+/**
+ * The entity representing a machine crafting recipes.
+ *
+ * @author BluePsyduck <bluepsyduck@gmx.com>
+ * @license http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+class Machine extends GenericEntity
+{
+    /**
+     * The crafting speed of the machine.
+     * @var float
+     */
+    protected $craftingSpeed = 1.;
+
+    /**
+     * The number of ingredient slots available in the machine.
+     * @var int
+     */
+    protected $numberOfIngredientSlots = 1;
+
+    /**
+     * The number of module slots available in the machine.
+     * @var int
+     */
+    protected $numberOfModuleSlots = 0;
+
+    /**
+     * The energy usage of the machine, in watt.
+     * @var int
+     */
+    protected $energyUsage = 0;
+
+    /**
+     * Returns the type of the entity.
+     * @return string
+     */
+    public function getType(): string
+    {
+        return EntityType::MACHINE;
+    }
+
+    /**
+     * Sets the crafting speed of the machine.
+     * @param float $craftingSpeed
+     * @return $this
+     */
+    public function setCraftingSpeed(float $craftingSpeed)
+    {
+        $this->craftingSpeed = $craftingSpeed;
+        return $this;
+    }
+
+    /**
+     * Returns the crafting speed of the machine.
+     * @return float
+     */
+    public function getCraftingSpeed(): float
+    {
+        return $this->craftingSpeed;
+    }
+
+    /**
+     * Sets the number of ingredient slots available in the machine.
+     * @param int $numberOfIngredientSlots
+     * @return $this
+     */
+    public function setNumberOfIngredientSlots(int $numberOfIngredientSlots)
+    {
+        $this->numberOfIngredientSlots = $numberOfIngredientSlots;
+        return $this;
+    }
+
+    /**
+     * Returns the number of ingredient slots available in the machine.
+     * @return int
+     */
+    public function getNumberOfIngredientSlots(): int
+    {
+        return $this->numberOfIngredientSlots;
+    }
+
+    /**
+     * Sets the number of module slots available in the machine.
+     * @param int $numberOfModuleSlots
+     * @return $this
+     */
+    public function setNumberOfModuleSlots(int $numberOfModuleSlots)
+    {
+        $this->numberOfModuleSlots = $numberOfModuleSlots;
+        return $this;
+    }
+
+    /**
+     * Returns the number of module slots available in the machine.
+     * @return int
+     */
+    public function getNumberOfModuleSlots(): int
+    {
+        return $this->numberOfModuleSlots;
+    }
+
+    /**
+     * Sets the energy usage of the machine, in watt.
+     * @param int $energyUsage
+     * @return $this
+     */
+    public function setEnergyUsage(int $energyUsage)
+    {
+        $this->energyUsage = $energyUsage;
+        return $this;
+    }
+
+    /**
+     * Returns the energy usage of the machine, in watt.
+     * @return int
+     */
+    public function getEnergyUsage(): int
+    {
+        return $this->energyUsage;
+    }
+
+    /**
+     * Writes the entity data to an array.
+     * @return array
+     */
+    public function writeData(): array
+    {
+        $data = array_merge(parent::writeData(), [
+            'craftingSpeed' => $this->craftingSpeed,
+            'numberOfIngredientSlots' => $this->numberOfIngredientSlots,
+            'numberOfModuleSlots' => $this->numberOfModuleSlots,
+            'energyUsage' => $this->energyUsage
+        ]);
+        unset($data['type']);
+        return $data;
+    }
+
+    /**
+     * Reads the entity data.
+     * @param DataContainer $data
+     * @return $this
+     */
+    public function readData(DataContainer $data)
+    {
+        parent::readData($data);
+        $this->craftingSpeed = $data->getFloat('craftingSpeed');
+        $this->numberOfIngredientSlots = $data->getInteger('numberOfIngredientSlots');
+        $this->numberOfModuleSlots = $data->getInteger('numberOfModuleSlots');
+        $this->energyUsage = $data->getInteger('energyUsage');
+        return $this;
+    }
+}
---
+++ test/src/Entity/MachineTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FactorioItemBrowserTest\Api\Client\Entity;
+
+use BluePsyduck\Common\Data\DataContainer;
+use FactorioItemBrowser\Api\Client\Entity\Machine;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * The PHPUnit test of the machine class.
+ *
+ * @author BluePsyduck <bluepsyduck@gmx.com>
+ * @license http://opensource.org/licenses/GPL-3.0 GPL v3
+ * @coversDefaultClass \FactorioItemBrowser\Api\Client\Entity\Machine
+ */
+class MachineTest extends TestCase
+{
+    /**
+     * Tests the constructing.
+     * @coversNothing
+     */
+    public function testConstruct()
+    {
+        $machine = new Machine();
+        $this->assertEquals('machine', $machine->getType());
+        $this->assertEquals('', $machine->getName());
+        $this->assertEquals('', $machine->getLabel());
+        $this->assertEquals('', $machine->getDescription());
+        $this->assertEquals(1., $machine->getCraftingSpeed());
+        $this->assertEquals(1, $machine->getNumberOfIngredientSlots());
+        $this->assertEquals(0, $machine->getNumberOfModuleSlots());
+        $this->assertEquals(0, $machine->getEnergyUsage());
+    }
+
+    /**
+     * Tests getting the type.
+     * @covers ::getType
+     */
+    public function testGetType()
+    {
+        $machine = new Machine();
+        $this->assertEquals('machine', $machine->getType());
+    }
+
+    /**
+     * Tests setting and getting the crafting speed.
+     * @covers ::setCraftingSpeed
+     * @covers ::getCraftingSpeed
+     */
+    public function testSetAndGetCraftingSpeed()
+    {
+        $machine = new Machine();
+        $this->assertEquals($machine, $machine->setCraftingSpeed(13.37));
+        $this->assertEquals(13.37, $machine->getCraftingSpeed());
+    }
+
+    /**
+     * Tests setting and getting the number of ingredient slots.
+     * @covers ::setNumberOfIngredientSlots
+     * @covers ::getNumberOfIngredientSlots
+     */
+    public function testSetAndGetNumberOfIngredientSlots()
+    {
+        $machine = new Machine();
+        $this->assertEquals($machine, $machine->setNumberOfIngredientSlots(42));
+        $this->assertEquals(42, $machine->getNumberOfIngredientSlots());
+    }
+
+    /**
+     * Tests setting and getting the number of module slots.
+     * @covers ::setNumberOfModuleSlots
+     * @covers ::getNumberOfModuleSlots
+     */
+    public function testSetAndGetNumberOfModuleSlots()
+    {
+        $machine = new Machine();
+        $this->assertEquals($machine, $machine->setNumberOfModuleSlots(42));
+        $this->assertEquals(42, $machine->getNumberOfModuleSlots());
+    }
+
+    /**
+     * Tests setting and getting the energy usage.
+     * @covers ::setEnergyUsage
+     * @covers ::getEnergyUsage
+     */
+    public function testSetAndGetEnergyUsage()
+    {
+        $machine = new Machine();
+        $this->assertEquals($machine, $machine->setEnergyUsage(1337));
+        $this->assertEquals(1337, $machine->getEnergyUsage());
+    }
+
+    /**
+     * Tests writing and reading the data.
+     * @covers ::writeData
+     * @covers ::readData
+     */
+    public function testWriteAndReadData()
+    {
+        $machine = new Machine();
+        $machine->setName('abc')
+                ->setLabel('def')
+                ->setDescription('ghi')
+                ->setCraftingSpeed(13.37)
+                ->setNumberOfIngredientSlots(42)
+                ->setNumberOfModuleSlots(21)
+                ->setEnergyUsage(1337);
+
+        $expectedData = [
+            'name' => 'abc',
+            'label' => 'def',
+            'description' => 'ghi',
+            'craftingSpeed' => 13.37,
+            'numberOfIngredientSlots' => 42,
+            'numberOfModuleSlots' => 21,
+            'energyUsage' => 1337
+        ];
+
+        $data = $machine->writeData();
+        $this->assertEquals($expectedData, $data);
+
+        $newMachine = new Machine();
+        $this->assertEquals($newMachine, $newMachine->readData(new DataContainer($data)));
+        $this->assertEquals($newMachine, $machine);
+    }
+}
