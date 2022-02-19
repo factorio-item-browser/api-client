5aa246dda1f14429497ad4865ea8ad7cdc7fa3cf
BluePsyduck <bluepsyduck@gmx.com>
2022-02-19 12:26:03 +0100

    Changed the recipe/details request to accept types and names.

--- src/Request/Recipe/RecipeDetailsRequest.php
+++ src/Request/Recipe/RecipeDetailsRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace FactorioItemBrowser\Api\Client\Request\Recipe;
 
 use FactorioItemBrowser\Api\Client\Request\AbstractRequest;
+use FactorioItemBrowser\Api\Client\Transfer\Entity;
 use JMS\Serializer\Annotation\Type;
 
 /**
@@ -16,8 +17,16 @@ use JMS\Serializer\Annotation\Type;
 class RecipeDetailsRequest extends AbstractRequest
 {
     /**
+     * The internal types and names of the recipes to return the details of.
+     * @var array<Entity>
+     */
+    #[Type('array<' . Entity::class . '>')]
+    public array $recipes = [];
+
+    /**
      * The internal names of the recipe to return the details of.
      * @var array<string>
+     * @deprecated
      */
     #[Type('array<string>')]
     public array $names = [];
--- test/serializer/Request/Recipe/RecipeDetailsRequestTest.php
+++ test/serializer/Request/Recipe/RecipeDetailsRequestTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace FactorioItemBrowserTestSerializer\Api\Client\Request\Recipe;
 
 use FactorioItemBrowser\Api\Client\Request\Recipe\RecipeDetailsRequest;
+use FactorioItemBrowser\Api\Client\Transfer\Entity;
 use FactorioItemBrowserTestSerializer\Api\Client\SerializerTestCase;
 
 /**
@@ -17,11 +18,23 @@ class RecipeDetailsRequestTest extends SerializerTestCase
 {
     public function test(): void
     {
+        $entity1 = new Entity();
+        $entity1->type = 'abc';
+        $entity1->name = 'def';
+        $entity2 = new Entity();
+        $entity2->type = 'ghi';
+        $entity2->name = 'jkl';
+
         $object = new RecipeDetailsRequest();
-        $object->names = ['abc', 'def'];
+        $object->recipes = [$entity1, $entity2];
+        $object->names = ['mno', 'pqr'];
 
         $data = [
-            'names' => ['abc', 'def'],
+            'recipes' => [
+                ['type' => 'abc', 'name' => 'def'],
+                ['type' => 'ghi', 'name' => 'jkl'],
+            ],
+            'names' => ['mno', 'pqr'],
         ];
 
         $this->assertSerialization($data, $object);
